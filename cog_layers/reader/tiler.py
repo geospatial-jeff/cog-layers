e8f0623a131012e40f18f3bb66bfd3bcbf1af3fa
Jeff Albrecht <geospatialjeff@gmail.com>
2025-02-27 21:56:55 -0700

    clean up a bit

--- cog_layers/reader/tiler.py
+++ cog_layers/reader/tiler.py
@@ -32,11 +32,9 @@ async def request_metatile(cog: Cog, tile: mercantile.Tile, size: int) -> list[l
     origin = mercantile.tile(*mercantile.ul(parent_tile), metatile_zoom)
     children = mercantile.children(tile, zoom=metatile_zoom)
     xs, ys = zip(*[(child.x - origin.x, child.y - origin.y) for child in children])
-    xmin = min(xs)
-    xmax = max(xs)
     unique_ys = list(set(ys))
     overview_level = metatile_zoom - parent_tile.z - 1
     return await asyncio.gather(*[
-        read_row(y, overview_level, cog, xmin, xmax) for y in unique_ys
+        read_row(y, overview_level, cog, min(xs), max(xs)) for y in unique_ys
     ])
 
