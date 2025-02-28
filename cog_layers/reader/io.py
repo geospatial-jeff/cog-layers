912ea0b84fb9515af819dde7e604741c92e7cefb
Jeff Albrecht <geospatialjeff@gmail.com>
2025-02-27 19:15:20 -0700

    expose clients

--- cog_layers/reader/io.py
+++ cog_layers/reader/io.py
@@ -1,19 +1,34 @@
 import os
 from dataclasses import dataclass
 import typing
+import functools
 
-import aiofiles
+import aiohttp
 import obstore as obs
 
 
-RangeRequestFuncType = typing.Callable[[str, str, int, int], typing.Awaitable[bytes]]
+RangeRequestFuncType = typing.Callable[[str, str, int, int, typing.Any], typing.Awaitable[bytes]]
 
 
-async def send_range_aiohttp(bucket, key, start, end) -> bytes:
+@functools.lru_cache(maxsize=1)
+def _get_default_aiohttp_client(**kwargs) -> aiohttp.ClientSession:
+    """Default aiohttp client with a singleton cache.
+    """
+    return aiohttp.ClientSession(**kwargs)
+
+
+async def send_range_aiohttp(bucket: str, key: str, start: int, end: int, client: typing.Any | None = None) -> bytes:
     raise NotImplementedError()
 
 
-async def send_range_obstore(bucket, key, start, end) -> bytes:
+@functools.lru_cache(maxsize=1)
+def _get_default_obstore_client(bucket_name: str, **kwargs) -> obs.store.S3Store:
+    """Default obstore client with a singleton cache."""
+    return obs.store.S3Store(bucket_name, **kwargs)
+
+
+async def send_range_obstore(bucket:str, key:str, start: int, end: int, client: typing.Any | None = None) -> bytes:
+    client = client or _get_default_obstore_client(bucket)
     store = obs.store.S3Store(
         bucket,
         config={"aws_default_region": "us-west-2", "aws_skip_signature": True}
